ac5d5875b2b715f8339a80d9c639544e337b9dbe
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-09-28 13:52:53 -0700

    killing this animaions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,40 +1,23 @@
 'use strict';
 
 angular.module('DF16Insurance')
-	.controller('MainCtrl', function ($scope, $state) {
+	.controller('MainCtrl', function ($scope, $state, $timeout) {
 		$scope.$state = $state;
+		$scope.delayFade = false;
 
-		$scope.buttonModel = [
-			{
-				name: 'buttonOne',
-				on: false,
-				text: '11:00am - 12:00pm'
-			},
-			{
-				name: 'buttonTwo',
-				on: false,
-				text: '1:00pm - 2:00pm'
-			},
-			{
-				name: 'buttonThree',
-				on: false,
-				text: '2:30pm - 3:30pm'
-			},
-			{
-				name: 'buttonFour',
-				on: false,
-				text: '4:00pm - 5:00pm'
-			}
-		];
-
-		$scope.toggleButtons = function(buttonName) {
-			for (var i = 0; i < $scope.buttonModel.length; i++) {
-				if ($scope.buttonModel[i].name === buttonName && $scope.buttonModel[i].on === false) {
-					$scope.buttonModel[i].on = true;
-				}
-				else {
-					$scope.buttonModel[i].on = false;
-				}
-			}
+		$scope.slideShare = function() {
+			$scope.imageStates[1].show = true;
 		};
+
+		$scope.goToInstagram = function() {
+			$scope.imageStates[2].show = true;
+			//Hide first camera view
+			$scope.imageStates[0].show = false;
+			//Start Animation
+			$timeout(function() {
+				$scope.delayFade = true;
+			}, 100);
+		};
+
+
 	});
