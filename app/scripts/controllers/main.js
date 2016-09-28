75983bdb5d617591d6176439f4b5eb03bf1dae10
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-09-28 14:50:48 -0700

    checkpoint boom

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,5 +19,34 @@ angular.module('DF16Insurance')
 			}, 100);
 		};
 
+		$scope.showNotification = function() {
+			$scope.imageStates[3].show = true;
+		};
 
+		$scope.showDetails = function() {
+			//Fade out notification
+			$scope.imageStates[3].show = false;
+			//Hide Share Screen
+			$scope.imageStates[1].show = false;
+			//Reveal new details screen behind current
+			$scope.imageStates[4].show = true;
+			//Fade out instagram image out
+			$scope.delayFade = false;
+			//Hide instagram
+			$timeout(function() {
+				$scope.imageStates[2].show = false;
+			}, 1000);
+		};
+
+		$scope.openCumulus = function() {
+			$scope.imageStates[5].show = true;
+			//Trigger the next stage automatically
+			$timeout(function() {
+				$scope.imageStates[6].show = true;
+			}, 2000);
+			//Nice, let's turn some layers off
+			$timeout(function() {
+				$scope.imageStates[4].show = false;
+			}, 500);
+		};
 	});
